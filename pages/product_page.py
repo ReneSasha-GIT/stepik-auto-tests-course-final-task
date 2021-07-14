8eb96408a6a314f7743777994144b5d88eb069ee
ReneSasha-GIT <renegat.sav@gmail.com>
2021-07-14 18:42:10 +0300

    Updated verification, so methods takes argument

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,14 +7,20 @@ class ProductPage(BasePage):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
 
-    def should_be_correct_add_title(self):
-        add_title = self.browser.find_element(*ProductPageLocators.ADD_TITLE)
+    def return_product_title(self):
         product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE)
-        assert add_title.text == product_title.text, f"Title in add-to-basket message is \"{add_title.text}\", but " \
-                                                     f"title on product page is \"{product_title.text}\""
+        return product_title.text
 
-    def should_be_correct_add_price(self):
-        add_price = self.browser.find_element(*ProductPageLocators.ADD_PRICE)
+    def return_product_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        assert add_price.text == product_price.text, f"Price in add-to-basket message is \"{add_price.text}\", but " \
-                                                     f"price on product page is \"{product_price.text}\""
+        return product_price.text
+
+    def should_be_correct_add_title(self, product_title):
+        add_title = self.browser.find_element(*ProductPageLocators.ADD_TITLE)
+        assert add_title.text == product_title, f"Title in add-to-basket message is \"{add_title.text}\", but " \
+                                                f"title on product page is \"{product_title}\""
+
+    def should_be_correct_add_price(self, product_price):
+        add_price = self.browser.find_element(*ProductPageLocators.ADD_PRICE)
+        assert add_price.text == product_price, f"Price in add-to-basket message is \"{add_price.text}\", but " \
+                                                     f"price on product page is \"{product_price}\""
--- test_product_page.py
+++ test_product_page.py
@@ -8,5 +8,5 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_correct_add_title()
-    page.should_be_correct_add_price()
+    page.should_be_correct_add_title(page.return_product_title())
+    page.should_be_correct_add_price(page.return_product_price())
