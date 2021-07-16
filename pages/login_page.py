7d46919a3dcbaaf4c4985db8c3f678f34952d33e
ReneSasha-GIT <renegat.sav@gmail.com>
2021-07-16 17:04:10 +0300

    Added setup fixture

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,19 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_fld = self.browser.find_element(*LoginPageLocators.EMAIL_REG)
+        email_fld.send_keys(email)
+        pass1_fld = self.browser.find_element(*LoginPageLocators.PASSWORD_1_REG)
+        pass1_fld.send_keys(password)
+        pass2_fld = self.browser.find_element(*LoginPageLocators.PASSWORD_2_REG)
+        pass2_fld.send_keys(password)
+        reg_btn = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        reg_btn.click()
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -11,10 +24,6 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, "word \"login\" not in url"
-
-    def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,35 @@
 import pytest
-from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+from pages.product_page import ProductPage
+import time
+
+
+@pytest.mark.product_user
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = "123ghj456!"
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_correct_add_title(page.return_product_title())
+        page.should_be_correct_add_price(page.return_product_price())
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
