edf131c4380628f6dadc0fdcbb49126447455850
ReneSasha-GIT <renegat.sav@gmail.com>
2021-07-16 13:36:03 +0300

    Added locators for basket page

--- pages/locators.py
+++ pages/locators.py
@@ -4,15 +4,21 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini .btn-default[href*=basket]")
 
 
-class MainPageLocators:
-    pass
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class MainPageLocators:
+    pass
 
 
 class ProductPageLocators:
